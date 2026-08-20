532b159cf7e51f0fe58f377bf761941d37143a48
“trinhvanlong” <vanlong232004@gmail.com>
2026-08-20 19:12:18 +0700

    Prepare deployment

--- DemoMVC/obj/Debug/net10.0/DemoMVC.AssemblyInfo.cs
+++ DemoMVC/obj/Debug/net10.0/DemoMVC.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DemoMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+422d86ea692c3324eb91539ba3dc33930517e642")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+65b8ea9a0153ae10eb5a45b060fc15a92b382ac4")]
 [assembly: System.Reflection.AssemblyProductAttribute("DemoMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DemoMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
